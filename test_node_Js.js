cef5b64957955c5731a3432d70069388dc89cffc
r0meroh <romeroh862@gmail.com>
2020-05-01 19:59:12 -0700

    added pictures made server run on a given port

--- test_node_Js.js
+++ test_node_Js.js
@@ -1,22 +1,22 @@
-//const http = require('http');
-//const challenge = who =>{
-   // console.log( who );
-//}
+const http = require('http');
+const challenge = who =>{
+    console.log( who );
+}
 
-//setTimeout(challenge,2*4000, ' hello');
-//setTimeout(challenge, 2*8000, ' hello');
+setTimeout(challenge,2*4000, ' hello');
+setTimeout(challenge, 2*8000, ' hello');
 
-//const server = http.createServer((req, res) => {
- //   res.end('Hello\n');
-//});
+const server = http.createServer((req, res) => {
+    res.end('Hello\n');
+});
 
-//server.listen(4242, () => {
-  //  console.log(('Server is running ....'));
-//});
+server.listen(4242, () => {
+    console.log(('Server is running ....'));
+});
 
-const print = require ('frame-print');
+//const print = require ('frame-print');
 
-print('hello npm');
+//print('hello npm');
 
 /*
 expected output:
